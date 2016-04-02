d87401f812c7a456936a184a6771c02500471105
cristiandavid <cristiandavid@Christia_cortes>
2016-04-02 00:59:07 -0500

    Ejercicio JAVAFX

---
+++ src/Browser.java
@@ -0,0 +1,19 @@
+
+import javafx.scene.Parent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cristiandavid
+ */
+class Browser extends Parent {
+
+    public Browser() {
+    }
+    
+}
